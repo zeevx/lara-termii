69f0b2a42f135ca2eb7378d3fa448229ee9f83fe
Adams Paul <adamsohiani@gmail.com>
2021-06-14 08:17:58 +0100

    Stable versitable version 0.1.3

--- src/LaraTermiiServiceProvider.php
+++ src/LaraTermiiServiceProvider.php
@@ -11,37 +11,7 @@ class LaraTermiiServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'lara-termii');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'lara-termii');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
 
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('lara-termii.php'),
-            ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/lara-termii'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/lara-termii'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/lara-termii'),
-            ], 'lang');*/
-
-            // Registering package commands.
-            // $this->commands([]);
-        }
     }
 
     /**
@@ -49,9 +19,6 @@ class LaraTermiiServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'lara-termii');
-
         // Register the main class to use with the facade
         $this->app->singleton('lara-termii', function () {
             return new LaraTermii;
